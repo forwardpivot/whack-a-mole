35916f1941202c28ed7142d0e9a4035418c3bb85
Taby <forwardpivot@proton.me>
2022-06-10 11:58:43 -0500

    Set up a function to change the images on a timer, but its not working. It reading the condition right, but not doing anything. I think its because I need to look at each image, and use changeImg on them. I'll try looping through them using a querySelectorAll, and then calling changeImg on each one

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ imgThree.addEventListener('click', changeImg);
 imgFour.addEventListener('click', changeImg);
 imgFive.addEventListener('click', changeImg);
 
+//This is for the buttons that dictate difficulty
+const easyBtn = document.querySelector('.easy');
+easyBtn.addEventListener('click', moleTimer);
 
 
 
@@ -23,15 +26,30 @@ imgFive.addEventListener('click', changeImg);
 
 
 
+//The function that toggles the image depending on the if it has mole or hole as a class name
 function changeImg(e) {
     const classList = e.target.classList;
     if (classList.contains('hole') ) {
         e.target.src = "img/mole.jpg";
         e.target.classList.add('mole');
         e.target.classList.remove('hole');
+        console.log("In the changeImg if");
     } else {
         e.target.src = "img/hole.jpg"
         e.target.classList.add('hole');
         e.target.classList.remove('mole');
+        console.log("In the changeImg else");
     }
+}
+
+function moleTimer(e) {
+    let sec = 3;
+    let timer = setInterval( () => {
+        console.log(sec);
+        sec--;
+        if (sec < 0) {
+            console.log("In the if");
+            changeImg(e);
+        }
+    },1000);
 }
