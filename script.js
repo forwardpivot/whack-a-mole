eb4db890c3207255db07758bc44c753be0041127
Taby <forwardpivot@proton.me>
2022-06-11 09:08:47 -0500

    I added a score keeper, and changed checkForPoint work with it. It does display the number on the screen, but it only goes to one. I think this is because I keep defining it in the loop, and setting it to zero again, and then it adds 1 to it.

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ easyBtn.addEventListener('click', moleTimer);
 //Made sec a global variable so that it can be change from inside functions
 let sec;
 
+//The variables for the score, and the timer
+const score = document.querySelector('.score');
+const gameTimer = document.querySelector('gameTimer');
+
+
+
 //The functions that toggles the image depending on the if it has mole or hole as a class name
 function changeImg(e) {
     const classList = e.target.classList;
@@ -54,6 +60,9 @@ function moleTimer(e) {
 function checkForPoint(e) {
     const classList = e.target.classList;
     if (classList.contains('mole')) {
-        console.log("You got a point!")
+        console.log("You hit a mole");
+        let highscore = 0;
+        highscore++;
+        score.textContent = highscore;
     }
 }
